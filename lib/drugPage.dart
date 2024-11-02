68ef02ee4adcb068fe43996dba989769c3cf9682
Alexandra Leonidova <alexandraleonid2002@gmail.com>
2024-11-02 20:02:49 +0500

    drug properties show

--- lib/drugPage.dart
+++ lib/drugPage.dart
@@ -1,36 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:testandroid/service/drug_service.dart';
 
-class DrugPage extends StatelessWidget {
+class DrugPage extends StatefulWidget {
   final String medicineName;
 
   const DrugPage({super.key, required this.medicineName});
 
   @override
+  State<DrugPage> createState() => _DrugPageState();
+}
+
+class _DrugPageState extends State<DrugPage> {
+  Map<String, dynamic>? drugData;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchDrugInfo();
+  }
+
+  Future<void> _fetchDrugInfo() async {
+    final data = await getDrugInfo(widget.medicineName);
+    setState(() {
+      drugData = data;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(medicineName),
+        title: Text(widget.medicineName),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: SingleChildScrollView(
+      body: drugData == null
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
         child: Column(
           children: [
             ExpansionTile(
               title: Text("Описание"),
               children: [
-                ListTile(title: Text("Здесь будет описание лекарства.")),
+                ListTile(title: Text(drugData?['description'] ?? 'Нет данных')),
               ],
             ),
             ExpansionTile(
               title: Text("Характеристики"),
               children: [
-                ListTile(title: Text("Здесь будут характеристики лекарства.")),
+                ListTile(
+                  title: Text(drugData?['influence'] ?? 'Нет данных'),
+                ),
+                ListTile(
+                  title: Text("ATX код: ${drugData?['atx_code'] ?? 'Нет данных'}"),
+                ),
               ],
             ),
             ExpansionTile(
               title: Text("Инструкция"),
               children: [
-                ListTile(title: Text("Здесь будет инструкция по применению.")),
+                ListTile(
+                  title: Text(drugData?['dosage'] ?? 'Нет данных'),
+                ),
+                ListTile(
+                  title: Text("Показания: ${drugData?['indication'] ?? 'Нет данных'}"),
+                ),
+                ListTile(
+                  title: Text("Побочные эффекты: ${drugData?['side_effects'] ?? 'Нет данных'}"),
+                ),
+              ],
+            ),
+            ExpansionTile(
+              title: Text("Активные вещества"),
+              children: [
+                for (var ingredient in drugData?['active_ingredients'] ?? [])
+                  ListTile(
+                    title: Text("${ingredient['name']}, дозировка: ${ingredient['dosage']}"),
+                  ),
               ],
             ),
           ],
--- lib/service/drug_service.dart
+++ lib/service/drug_service.dart
@@ -1,23 +1,21 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<void> getDrugInfo(String name) async {
+Future<Map<String, dynamic>?> getDrugInfo(String name) async {
   final url = Uri.parse('http://b257-188-17-211-72.ngrok-free.app/v1/drug/$name');
 
   try {
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      print("Drug name: ${data['name']}");
-      print("Active ingredients:");
-      for (var ingredient in data['active_ingredients']) {
-        print("- ${ingredient['name']}, dosage: ${ingredient['dosage']}");
-      }
+      final decodedBody = utf8.decode(response.bodyBytes);
+      return jsonDecode(decodedBody);
     } else {
       print("Error: ${response.statusCode}");
+      return null;
     }
   } catch (e) {
     print("Failed to fetch drug info : $e");
+    return null;
   }
 }
